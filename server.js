edb65d5ed4b38a312923c342ed086ed2cc80e328
Anirudh Thakur <anirudh.thakur94@gmail.com>
2020-04-18 21:09:42 +0530

    Bug fix

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ var path = require('path');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/html/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.get('/game', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/html/game.html'));
+    res.sendFile(path.join(__dirname + '/game.html'));
 });
 app.get('/game2', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/html/game2.html'));
+    res.sendFile(path.join(__dirname + '/game2.html'));
 });
 
 
